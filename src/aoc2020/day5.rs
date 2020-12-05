ebf28b5bc706b5a26eb85fe276a111daffef3367
gokberkkocak <gokberkkocak@gmail.com>
2020-12-05 15:38:27 +0000

    Update day5.rs

--- src/aoc2020/day5.rs
+++ src/aoc2020/day5.rs
@@ -4,8 +4,8 @@ pub fn run() {
     let input = get_puzzle_input(2020, 5);
     let p1 = part1(&input);
     let p2 = part2(&input);
-    println!("{}", p1);
-    println!("{}", p2);
+    println!("p1 {}", p1);
+    println!("p1 {}", p2);
 }
 
 pub fn part1(input: &str) -> usize {
@@ -13,28 +13,27 @@ pub fn part1(input: &str) -> usize {
 }
 
 pub fn part2(input: &str) -> usize {
-    let max = input.lines().map(|x| get_seat_value(x)).max().unwrap();
-    let min = input.lines().map(|x| get_seat_value(x)).min().unwrap();
-    let sum = input.lines().map(|x| get_seat_value(x)).sum::<usize>();
+    let seats = input.lines().map(|x| get_seat_value(x)).collect::<Vec<_>>();
+    let max = seats.iter().max().unwrap();
+    let min = seats.iter().min().unwrap();
+    let sum = seats.iter().sum::<usize>();
     (max * (max + 1) / 2) - sum - ((min - 1) * min / 2)
 }
 
 fn get_seat_value(entry: &str) -> usize {
+    fn get_seat_inner_value(value: &str, bit_indicator: char) -> usize {
+        value
+            .chars()
+            .rev()
+            .enumerate()
+            .filter(|(_i, c)| *c == bit_indicator)
+            .map(|(i, _c)| usize::pow(2, i as u32))
+            .sum::<usize>()
+    }
+
     let (row_str, col_str) = entry.split_at(7);
-    let row = row_str
-        .chars()
-        .rev()
-        .enumerate()
-        .filter(|(_i, c)| *c == 'B')
-        .map(|(i, _c)| usize::pow(2, i as u32))
-        .sum::<usize>();
-    let col = col_str
-        .chars()
-        .rev()
-        .enumerate()
-        .filter(|(_i, c)| *c == 'R')
-        .map(|(i, _c)| usize::pow(2, i as u32))
-        .sum::<usize>();
+    let row = get_seat_inner_value(row_str, 'B');
+    let col = get_seat_inner_value(col_str, 'R');
     let seat = row * 8 + col;
     seat
 }
