f5d8805331a1b312c23a04ec5f89f2d008d62691
Gökberk KOÇAK <gk34@st-andrews.ac.uk>
2021-12-01 13:49:42 +0000

    Add 2021 day 1

--- src/aoc2021/day1.rs
+++ src/aoc2021/day1.rs
@@ -1,3 +1,47 @@
 pub(crate) fn run() {
-    todo!()
+    let input = crate::util::get_puzzle_input(2021, 1);
+    let v = parse(&input);
+    let p1 = part1(&v);
+    println!("p1: {} ", p1);
+    let p2 = part2(&v);
+    println!("p2: {} ", p2);
+}
+
+fn parse(input: &str) -> Vec<u32> {
+    input.lines().map(|l| l.parse().unwrap()).collect()
+}
+
+fn part1(v: &[u32]) -> usize {
+    v.iter()
+        .zip(v.iter().skip(1))
+        .filter(|(a, b)| b > a)
+        .count()
+}
+
+fn part2(v: &[u32]) -> usize {
+    v.iter()
+        .zip(v.iter().skip(3))
+        .filter(|(a, b)| b > a)
+        .count()
+}
+
+#[cfg(test)]
+mod tests {
+
+    use super::*;
+    #[test]
+    fn test_1() {
+        let input = crate::util::read_file("inputs/2021_1_test.in");
+        let v = parse(&input);
+        let p1 = part1(&v);
+        assert_eq!(p1, 7);
+    }
+
+    #[test]
+    fn test_2() {
+        let input = crate::util::read_file("inputs/2021_1_test.in");
+        let v = parse(&input);
+        let p2 = part2(&v);
+        assert_eq!(p2, 5);
+    }
 }
