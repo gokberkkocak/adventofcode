10d37d9d0d136eb65d1966116945f799f6795bdc
Gökberk KOÇAK <gk34@st-andrews.ac.uk>
2021-12-07 14:13:33 +0000

    Add 2021 day 7

--- src/aoc2021/day7.rs
+++ src/aoc2021/day7.rs
@@ -1,3 +1,65 @@
 pub(crate) fn run() {
-    todo!()
+    let input = crate::util::get_puzzle_input(2021, 7);
+    let mut array = parse(&input);
+    array.sort();
+    let p1 = part1(&array);
+    println!("Part 1: {}", p1);
+    let p2 = part2(&array);
+    println!("Part 2: {}", p2);
+}
+
+fn parse(input: &str) -> Vec<isize> {
+    input.split(",").map(|s| s.parse().unwrap()).collect()
+}
+
+fn median(array: &[isize]) -> isize {
+    if (array.len() % 2) == 0 {
+        let ind_left = array.len() / 2 - 1;
+        let ind_right = array.len() / 2;
+        ((array[ind_left] + array[ind_right]) / 2) as isize
+    } else {
+        array[(array.len() / 2)]
+    }
+}
+
+fn mean(array: &[isize]) -> isize {
+    (array.iter().sum::<isize>() as f64 / array.len() as f64).round() as isize
+}
+
+fn part1(sorted_array: &[isize]) -> isize {
+    let median = median(sorted_array);
+    sorted_array.iter().map(|x| (x - median).abs()).sum()
+}
+
+fn part2(array: &[isize]) -> isize {
+    let mean = mean(array);
+    array
+        .iter()
+        .map(|x| {
+            let diff = (x - mean).abs();
+            diff * (diff + 1) / 2
+        })
+        .sum()
+}
+
+#[cfg(test)]
+mod tests {
+
+    use super::*;
+    #[test]
+    fn test_1() {
+        let input = crate::util::read_file("inputs/2021_7_test.in");
+        let mut array = parse(&input);
+        array.sort();
+        let p1 = part1(&array);
+        assert_eq!(p1, 37);
+    }
+
+    #[test]
+    fn test_2() {
+        let input = crate::util::read_file("inputs/2021_7_test.in");
+        let array = parse(&input);
+        let p1 = part2(&array);
+        assert_eq!(p1, 168);
+    }
 }
