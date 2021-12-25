767e6929729d1604197b9710124354f00df35f2b
Gokberk Kocak <gk34@st-andrews.ac.uk>
2021-12-25 15:07:01 +0000

    Update day25.rs
    remove extra alloc

--- src/aoc2021/day25.rs
+++ src/aoc2021/day25.rs
@@ -13,10 +13,16 @@ fn parse(input: &str) -> SeaBed {
             .lines()
             .map(|l| {
                 l.bytes()
-                    .map(|b| match b {
-                        b'>' => SeaBedSpace::EastFaced,
-                        b'v' => SeaBedSpace::SouthFaced,
-                        _ => SeaBedSpace::Empty,
+                    .map(|b| {
+                        let space = match b {
+                            b'>' => SeaBedSpace::EastFaced,
+                            b'v' => SeaBedSpace::SouthFaced,
+                            _ => SeaBedSpace::Empty,
+                        };
+                        SeaBedState {
+                            space,
+                            moved: false,
+                        }
                     })
                     .collect()
             })
@@ -24,7 +30,12 @@ fn parse(input: &str) -> SeaBed {
     )
 }
 
-struct SeaBed(Vec<Vec<SeaBedSpace>>);
+struct SeaBed(Vec<Vec<SeaBedState>>);
+#[derive(Clone, PartialEq)]
+struct SeaBedState {
+    space: SeaBedSpace,
+    moved: bool,
+}
 
 #[derive(Clone, PartialEq)]
 enum SeaBedSpace {
@@ -37,10 +48,10 @@ impl SeaBed {
     fn apply_steps(&mut self) -> usize {
         let mut count = 0;
         loop {
-            let move1 = self.step(0, 1);
-            let move2 = self.step(1, 0);
+            let move_x = self.step(0, 1);
+            let move_y = self.step(1, 0);
             count += 1;
-            if !(move1 || move2) {
+            if !(move_x || move_y) {
                 break;
             }
         }
@@ -49,25 +60,63 @@ impl SeaBed {
 
     fn step(&mut self, dy: usize, dx: usize) -> bool {
         let (y_max, x_max) = (self.0.len(), self.0[0].len());
-        let mut new_bed = vec![vec![SeaBedSpace::Empty; x_max]; y_max];
         let mut moved = false;
         for (y, x) in (0..y_max).cartesian_product(0..x_max) {
             match self.0[y][x] {
-                SeaBedSpace::EastFaced if self.0[y][(x + dx) % x_max] == SeaBedSpace::Empty => {
-                    new_bed[y][(x + dx) % x_max] = SeaBedSpace::EastFaced;
+                SeaBedState {
+                    space: SeaBedSpace::EastFaced,
+                    moved: false,
+                } if self.0[y][(x + dx) % x_max]
+                    == SeaBedState {
+                        space: SeaBedSpace::Empty,
+                        moved: false,
+                    } =>
+                {
+                    self.0[y][(x + dx) % x_max].space = SeaBedSpace::EastFaced;
+                    self.0[y][(x + dx) % x_max].moved = true;
+                    self.0[y][x].space = SeaBedSpace::Empty;
+                    self.0[y][x].moved = true;
                     moved = true;
                 }
-                SeaBedSpace::SouthFaced if self.0[(y + dy) % y_max][x] == SeaBedSpace::Empty => {
-                    new_bed[(y + dy) % y_max][x] = SeaBedSpace::SouthFaced;
+                SeaBedState {
+                    space: SeaBedSpace::SouthFaced,
+                    moved: false,
+                } if self.0[(y + dy) % y_max][x]
+                    == SeaBedState {
+                        space: SeaBedSpace::Empty,
+                        moved: false,
+                    } =>
+                {
+                    self.0[(y + dy) % y_max][x].space = SeaBedSpace::SouthFaced;
+                    self.0[(y + dy) % y_max][x].moved = true;
+                    self.0[y][x].space = SeaBedSpace::Empty;
+                    self.0[y][x].moved = true;
                     moved = true;
                 }
-                SeaBedSpace::EastFaced => new_bed[y][x] = SeaBedSpace::EastFaced,
-                SeaBedSpace::SouthFaced => new_bed[y][x] = SeaBedSpace::SouthFaced,
                 _ => {}
             }
         }
-        self.0 = new_bed;
+
+        self.0
+            .iter_mut()
+            .for_each(|row| row.iter_mut().for_each(|s| s.moved = false));
         moved
+    }
+}
+
+impl core::fmt::Display for SeaBed {
+    fn fmt(&self, f: &mut core::fmt::Formatter) -> core::fmt::Result {
+        for row in &self.0 {
+            for s in row {
+                match s.space {
+                    SeaBedSpace::EastFaced => write!(f, ">")?,
+                    SeaBedSpace::SouthFaced => write!(f, "v")?,
+                    _ => write!(f, ".")?,
+                }
+            }
+            write!(f, "\n")?;
+        }
+        Ok(())
     }
 }
 
