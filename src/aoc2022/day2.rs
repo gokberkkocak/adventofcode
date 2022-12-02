23d290fc94d2fbb4f0e9a8cc0782754993321f4a
Gökberk KOÇAK <4727301+gokberkkocak@users.noreply.github.com>
2022-12-02 16:31:50 +0000

    Add 2022 day 2

--- src/aoc2022/day2.rs
+++ src/aoc2022/day2.rs
@@ -1,3 +1,43 @@
-pub fn run(){
-    todo!()
+pub fn run() {
+    let input = crate::util::get_puzzle_input(2022, 2);
+    let p1 = part1(&input);
+    println!("p1: {}", p1);
+    let p2 = part2(&input);
+    println!("p1: {}", p2);
+}
+
+fn part1(input: &str) -> i32 {
+    input
+        .lines()
+        .map(|l| l.as_bytes())
+        .map(|b| ((b[0] as u8 - b'A') as i32, (b[2] as u8 - b'X') as i32))
+        .map(|(op, me)| (1 + me + 3 * (1 + me - op).rem_euclid(3)))
+        .sum()
+}
+
+fn part2(input: &str) -> i32 {
+    input
+        .lines()
+        .map(|l| l.as_bytes())
+        .map(|b| ((b[0] as u8 - b'A') as i32, (b[2] as u8 - b'X') as i32))
+        .map(|(op, win)| (1 + (op + win + 2) % 3 + 3 * win))
+        .sum()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn test_1() {
+        let input = crate::util::read_file("inputs/2022_2_test.in");
+        let p1 = part1(&input);
+        assert_eq!(p1, 15);
+    }
+
+    #[test]
+    fn test_2() {
+        let input = crate::util::read_file("inputs/2022_2_test.in");
+        let p2 = part2(&input);
+        assert_eq!(p2, 12);
+    }
 }
