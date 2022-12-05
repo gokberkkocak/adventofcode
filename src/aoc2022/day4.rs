7e1680ce8d4872c530740a1bb7ee90883c68d78f
Gökberk KOÇAK <4727301+gokberkkocak@users.noreply.github.com>
2022-12-05 17:45:27 +0000

    refactor day 4

--- src/aoc2022/day4.rs
+++ src/aoc2022/day4.rs
@@ -6,41 +6,29 @@ pub fn run() {
     println!("p2: {}", p2);
 }
 
+pub fn parse(input: &str) -> impl Iterator<Item = (u8, u8, u8, u8)> + '_ {
+    input.lines().map(|pair_str| {
+        let pairs = pair_str.split(',').collect::<Vec<_>>();
+        let elf_1 = pairs[0]
+            .split('-')
+            .map(|i| i.parse::<u8>().unwrap())
+            .collect::<Vec<_>>();
+        let elf_2 = pairs[1]
+            .split('-')
+            .map(|i| i.parse::<u8>().unwrap())
+            .collect::<Vec<_>>();
+        (elf_1[0], elf_1[1], elf_2[0], elf_2[1])
+    })
+}
+
 pub fn part1(input: &str) -> u64 {
-    input
-        .lines()
-        .map(|pair_str| {
-            let pairs = pair_str.split(',').collect::<Vec<_>>();
-            let elf_1 = pairs[0]
-                .split('-')
-                .map(|i| i.parse::<u8>().unwrap())
-                .collect::<Vec<_>>();
-            let elf_2 = pairs[1]
-                .split('-')
-                .map(|i| i.parse::<u8>().unwrap())
-                .collect::<Vec<_>>();
-            (elf_1[0], elf_1[1], elf_2[0], elf_2[1])
-        })
+    parse(&input)
         .filter(|(a, b, m, n)| (m <= a && b <= n) || (a <= m && n <= b))
         .count() as u64
 }
 
 pub fn part2(input: &str) -> u64 {
-    input
-        .lines()
-        .map(|pair_str| {
-            let pairs = pair_str.split(',').collect::<Vec<_>>();
-            let elf_1 = pairs[0]
-                .split('-')
-                .map(|i| i.parse::<u8>().unwrap())
-                .collect::<Vec<_>>();
-            let elf_2 = pairs[1]
-                .split('-')
-                .map(|i| i.parse::<u8>().unwrap())
-                .collect::<Vec<_>>();
-
-            (elf_1[0], elf_1[1], elf_2[0], elf_2[1])
-        })
+    parse(&input)
         .filter(|(a, b, m, n)| (a <= m && m <= b) || (m <= a && a <= n))
         .count() as u64
 }
