247ab19aae8652d6e61fd7bca39a31003844eb86
Gokberk Kocak <gokberkkocak@gmail.com>
2025-12-10 17:07:35 +0000

    refactor: day 6 2025

--- src/aoc2025/day6.rs
+++ src/aoc2025/day6.rs
@@ -6,7 +6,7 @@ pub(crate) fn run() {
     println!("part 2: {}", p2);
 }
 
-fn part1(input: &str) -> usize{
+fn part1(input: &str) -> usize {
     let batches = parse_input_naive(input);
     batches.iter().map(|batch| batch.calculate()).sum()
 }
@@ -17,19 +17,22 @@ fn part2(input: &str) -> usize {
 }
 
 fn parse_input_naive(input: &str) -> Vec<Batch> {
-    let len = input.lines().next().unwrap().split_whitespace().count();
-    let mut batches = vec![Batch::new_empty(); len];
+    let nb_batches = input.lines().next().unwrap().split_whitespace().count();
+    let mut batches = vec![Batch::new_empty(); nb_batches];
 
-    for line in input.lines().take(input.lines().count() - 1) {
+    input.lines().rev().skip(1).for_each(|line| {
         line.split_ascii_whitespace()
             .zip(batches.iter_mut())
             .for_each(|(part, batch)| {
                 let num: usize = part.parse().unwrap();
                 batch.items.push(num);
-            });
-    }
-    let last_line = input.lines().last().unwrap();
-    last_line
+            })
+    });
+
+    input
+        .lines()
+        .last()
+        .unwrap()
         .split_ascii_whitespace()
         .zip(batches.iter_mut())
         .for_each(|(part, batch)| match part {
@@ -40,43 +43,44 @@ fn parse_input_naive(input: &str) -> Vec<Batch> {
     batches
 }
 
-// quick dirty solution in less than 10 minutes
+// quick dirty solution using transposition of the input grid
 fn parse_input_transpose(input: &str) -> Vec<Batch> {
-    // put every char in a 2d char array except last line
-    let mut grid: Vec<Vec<char>> = vec![];
-    for line in input.lines().take( input.lines().count() - 1 ) {
-        let chars: Vec<char> = line.chars().collect();
-        grid.push(chars);
-    }
+    let line_len = input.lines().next().unwrap().len();
+    let mut line_iterator = input
+        .lines()
+        .take(input.lines().count() - 1)
+        .map(|n| n.chars().into_iter())
+        .collect::<Vec<_>>();
 
-    // transpose the grid
-    let mut transposed: Vec<Vec<char>> = vec![];
-    for col in 0..grid[0].len() {
-        let mut new_row: Vec<char> = vec![];
-        for row in 0..grid.len() {
-            new_row.push(grid[row][col]);
-        }
-        transposed.push(new_row);
-    }
+    let transposed = (0..line_len)
+        .map(|_| {
+            line_iterator
+                .iter_mut()
+                .map(|n| n.next().unwrap())
+                .collect::<Vec<_>>()
+        })
+        .collect::<Vec<_>>();
 
-    // convert transposed into multi line string again
-    let mut transposed_str = String::new();
+    // read transposed grid and construct batches
+    let nb_batches = input.lines().next().unwrap().split_whitespace().count();
+    let mut batches = vec![Batch::new_empty(); nb_batches];
+    let mut current_batch_idx = 0;
+
     for row in transposed {
         let line: String = row.into_iter().collect();
         if line.trim().is_empty() {
-            transposed_str.push('\n');
+            current_batch_idx += 1;
         } else {
-            transposed_str.push_str(&line);
-            transposed_str.push(' ');
+            batches[current_batch_idx]
+                .items
+                .push(line.trim().parse().unwrap());
         }
     }
-    let mut batches = vec![Batch::new_empty(); transposed_str.lines().count()];
-    for (line, batch) in transposed_str.lines().zip(batches.iter_mut()) {
-        batch.items = line.split_ascii_whitespace().map(|s| s.parse().unwrap()).collect();   
-    }
 
-    let last_line = input.lines().last().unwrap();
-    last_line
+    input
+        .lines()
+        .last()
+        .unwrap()
         .split_ascii_whitespace()
         .zip(batches.iter_mut())
         .for_each(|(part, batch)| match part {
@@ -85,7 +89,6 @@ fn parse_input_transpose(input: &str) -> Vec<Batch> {
             _ => panic!("Unknown operation"),
         });
     batches
-    
 }
 
 #[derive(Debug, Clone, Copy)]
